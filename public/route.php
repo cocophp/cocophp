5bae737ec36d2a54024676b35b665abbc32c821f
Jan <zhaogaojian@scrmtech.com>
2019-09-24 07:53:52 +0800

    版本修整

--- public/route.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    '/'   => "index/index/index",
-    'doc' => 'index/document/index/index',
-    'doc/([a-zA-Z]+)' => 'index/document/index/$1',
-    'doc/([a-zA-Z]+)/(\d+)' => 'index/document/index/$1?id=$2',
-];
