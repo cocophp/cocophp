b730c468e691f7c80d05ec52cfd8a327995bbc29
cocophp <jan@Jan.cc>
2019-11-02 12:04:54 +0800

    增加php5的抛错

--- core/Cores.php
+++ core/Cores.php
@@ -11,7 +11,11 @@ class Cores{
             $request = Config::get( 'system.request' );
             $contro  = "{$request['modules']}\\{$request['contros']}";
             $action  = Config::get( 'system.request.action' );
-            $contro  = new $contro();
+	    $contro  = new $contro();
+	    // php5的情况下，拿不到方法也抛不出异常。只能手动判断
+            if( !method_exists( $contro, $action ) ){
+                exit( "function $action not found" );
+            }
             $res     = $contro->$action();
             if( is_string( $res ) or is_numeric( $res ) ){
                 return $res;
--- index.php
+++ index.php
@@ -4,9 +4,10 @@
 // 最简单的路由转发.让service更轻量.
 spl_autoload_register( function( $class ){
     $file = __DIR__ .'/' . str_replace( '\\' , '/', $class ) . '.php';
-    if ( file_exists( $file ) ) {
-        require_once $file;
+    if ( !file_exists( $file ) ) {
+	throw new \Exception( "class $class not found", 1 );
     }
+    require_once $file;
     // var_dump( get_included_files() );
 } );
 
